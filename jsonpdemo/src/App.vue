e39af52097b06c2fd2f9e7592dd0ea1ddd407283
LoongRay <1364146534@qq.com>
2021-03-09 00:18:10 +0800

    jsonp跨域

---
+++ jsonpdemo/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import jsonp from 'jsonp'
+export default {
+  name: 'App',
+  data(){
+    return{
+      data:''
+    }
+  },
+  mounted () {
+    let url = "https://www.imooc.com/activity/newcomer";
+    jsonp(url,(err,res)=>{
+      let result = res;
+      this.data = result;
+      console.log(result);
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
