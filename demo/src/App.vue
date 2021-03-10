cfc03273debe7251c77b79daf303186dca6d8a2f
LoongRay <1364146534@qq.com>
2021-03-10 18:04:32 +0800

    提交代理服务器文件

---
+++ demo/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import jsonp from "jsonp";
+import axios from "axios"
+export default {
+  name: "App",
+  data() {
+    return {
+      data: {},
+    };
+  },
+  mounted() {
+    //用jsonp代理服务器实现跨域
+    let url = "/activity/servicetime";
+    // jsonp(url,(err,res)=>{
+    //   let result = res;
+    //   this.data = result;
+    //   console.log(result);
+    // })
+
+    axios.get(url)
+      .then(res=>{console.log(res)})
+      .catch(err => {console.log(err)});
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo/src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+
+//配置服务器代理跨域
+axios.defaults.baseURL = '/api'  //关键代码:在此添加api前缀,不用每次输入请求地址时输入/api
+Vue.config.productionTip = false
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
